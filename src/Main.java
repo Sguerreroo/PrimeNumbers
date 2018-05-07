7231135ad46a3a89367448baf2ebabcbc7414acf
Samuel <samugm1997@gmail.com>
2018-05-07 09:46:11 +0100

    test added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(PrimeNumbers.isPrime(17, 12));
-        System.out.println(PrimeNumbers.isPrime(50, 30));
-        System.out.println(PrimeNumbers.isPrime(17, 16));
+        int m = 2;
+        int trueCount = 0;
+        int primes[] = {17,31,191,607,727,524287};
+        int composites[] = {20,100,585,657,100096};
+
+        System.out.println("Prueba números primos");
+
+
+        for (int i = 0; i < primes.length; i++) {
+            trueCount = Main.test(primes[i], m);
+            System.out.println("Acierto " + trueCount + "% para " + primes[i]);
+        }
+
+        System.out.println("Prueba números compuestos");
+        for (int i = 0; i < composites.length; i++) {
+            trueCount = Main.test(composites[i], m);
+            System.out.println("Acierto " + (100-trueCount) + "% para " + composites[i]);
+        }
+
+
     }
+
+    private static int test (int n, int m){
+        int trueCount= 0;
+        for (int i = 0; i < 100; i++) {
+            if(PrimeNumbers.isPrime(n, m))
+                trueCount++;
+        }
+        return trueCount;
+    }
+
 
 }
